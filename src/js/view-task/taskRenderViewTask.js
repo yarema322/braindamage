33c4f6f447a99c8e6e1a0ba242c0bff8be421ad4
yarema322 <yaremchuk19071999@gmail.com>
2025-12-29 19:26:41 +0200

    update view-task rendering and styles

---
+++ src/js/view-task/taskRenderViewTask.js
@@ -0,0 +1,101 @@
+// id from URL
+function getTaskIdFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    return params.get("id");
+}
+
+// get task from LocalStorage
+function getTasksFromStorage() {
+    const data = localStorage.getItem("tasks");
+    return data ? JSON.parse(data) : [];
+}
+
+// get task by id
+function getTaskById(id) {
+    const tasks = getTasksFromStorage();
+    return tasks.find(task => task.id === id);
+}
+
+// process of rendering
+document.addEventListener("DOMContentLoaded", () => {
+    const taskId = getTaskIdFromUrl();
+    if (!taskId) return;
+
+    const task = getTaskById(taskId);
+
+    renderViewTask(task);
+});
+
+// render task
+const containerViewTask = document.querySelector(".view-task");
+const template = document.getElementById("view-task-template");
+
+function renderViewTask(task) {
+    containerViewTask.innerHTML = "";
+
+    const fragment = template.content.cloneNode(true);
+
+    // img
+    const img = fragment.querySelector(".view-task__header-img");
+    if (task.image) {
+        img.src = task.image;
+    } else {
+        img.remove();
+    }
+
+    // task title
+    fragment.querySelector(".view-task__title").textContent = task.title;
+    // task priority
+    const priorityViewTask = fragment.querySelector(".view-task__priority");
+    
+    if (task.priority) {
+        priorityViewTask.innerHTML = `
+            Priority: <span class="view-task__priority--${task.priority}">
+                ${formatPriority(task.priority)}
+            </span>
+            `;
+    } else {
+        priorityViewTask.remove();
+    }
+    // task status
+
+    const statusViewTask = fragment.querySelector(".view-task__status");
+
+    if (task.status) {
+        statusViewTask.innerHTML = `
+            Status: <span class="view-task__status--${task.status}">
+                ${formatStatus(task.status)}
+            </span>
+            `;
+    } else {
+        statusViewTask.remove();
+    }
+    // task date
+    fragment.querySelector(".view-task__date").textContent =
+        `Created on: ${task.createdAt}`;
+    
+    // task description
+    fragment.querySelector(".view-task__description").textContent = task.description;
+
+    containerViewTask.append(fragment);
+}
+
+// format priority
+function formatPriority(priority) {
+    const map = {
+        extreme: "Extreme",
+        moderate: "Moderate",
+        low: "Low",
+    };
+    return map[priority] || priority;
+}
+
+// format status
+function formatStatus(status) {
+  const map = {
+    "not-started": "Not Started",
+    "in-progress": "In Progress",
+    "low": "Low",
+  };
+  return map[status] || status;
+}
--- src/js/view-task/view-task.js
+++ src/js/view-task/view-task.js
@@ -4,6 +4,13 @@ import "../common/dateFull.js";
 import "../common/modalWindow.js";
 import "../common/modalForm.js";
 
+
+import "../view-task/taskRenderViewTask.js";
+
+import "../common/historyBackButton.js";
+
+
+
 import { showFileNames, initFileUpload } from "../common/filePreview.js";
 import { throttle } from "../common/throttle.js";
 import { initHamMenu } from "../common/hamMenu.js";
