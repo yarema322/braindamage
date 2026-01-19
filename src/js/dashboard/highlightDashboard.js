d6bf0d407338b15591d2b3997e0198e83733fb48
yarema322 <yaremchuk19071999@gmail.com>
2026-01-19 17:42:24 +0200

    chore: delete unused util

--- src/js/dashboard/highlightDashboard.js
+++
@@ -1,6 +0,0 @@
-if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
-        const dashboardItem = document.querySelector(
-          ".sidebar__item"
-        );
-        dashboardItem.classList.add("sidebar__item--selected");
-      }
