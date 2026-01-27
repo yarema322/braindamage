90e9998ede54f069f10f03fc51cc1da5dd2d55ce
yarema322 <yaremchuk19071999@gmail.com>
2026-01-27 16:45:50 +0200

    fix: task progress calculation and visualization

--- src/js/dashboard/taskProgress.js
+++ src/js/dashboard/taskProgress.js
@@ -1,6 +1,18 @@
+import { getTasksFromStorage } from "../common/storage.js";
+
+const tasks = getTasksFromStorage();
+const total = tasks.length || 1;
+
+const percentOfNotStarted = Math.round((tasks.filter(task => task.status === "not-started").length / total)) * 100;
+const percentOfInProgress = Math.round((tasks.filter(task => task.status === "in-progress").length / total)) * 100;
+const percentOfCompleted = Math.round((tasks.filter(task => task.status === "completed").length / total)) * 100;
+  
+
 function setProgress(id, percent) {
         const circle = document.getElementById(id);
         const label = document.getElementById("percent-" + id.split("-")[1]);
+        if (!circle || !label) return;
+
         const radius = 45;
         const circumference = 2 * Math.PI * radius;
 
@@ -11,7 +23,8 @@ function setProgress(id, percent) {
         label.textContent = `${percent}%`;
       }
 
-      // znachenie progressa
-      setProgress("progress-completed", 84);
-      setProgress("progress-inprogress", 46);
-      setProgress("progress-notstarted", 13);
+      setProgress("progress-completed", percentOfCompleted);
+      setProgress("progress-inprogress", percentOfInProgress);
+      setProgress("progress-notstarted", percentOfNotStarted);
+
+      
